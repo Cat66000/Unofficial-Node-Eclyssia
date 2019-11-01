893d47e385e84afcaf239e589b154aa2e2af80d7
Cat66000 <yoanguardiola@outlook.com>
2019-11-01 02:50:28 +0100

    Replace Error with a custom error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const wump = require("wumpfetch");
+const Err = require("./EclyssiaError.js/index.js");
+
 const baseURL = "https://eclyssia-api.tk/api/v1/";
 
 const get = async (endpoint = "", url = "") => {
     const data = await wump(`${baseURL}${endpoint}?url=${url}`).send();
 
     if (data.statusCode !== 200) {
-        throw new Error(data);
+        throw new Err(data);
     }
 
     return data.buffer();
