4d596aa698af46d0b5611d3c7cbc3419b8719d62
Cat66000 <yoanguardiola@outlook.com>
2019-11-01 14:35:33 +0100

    Update files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    get: require("./src/index.js"),
+    get: require("./src/index.js").get,
+    endpoints: require("./src/index.js").endpoints,
     version: require("./package.json").version
 };
--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ const Err = require("./EclyssiaError.js");
 
 const baseURL = "https://eclyssia-api.tk/api/v1/";
 
+const endpoints = {
+    filters: [
+        "blur",
+        "greyscale",
+        "invert",
+        "pixelate",
+        "posterize",
+        "sepia"
+    ],
+    image: [
+
+    ]
+};
+
 // username is only required by phvideo & captcha endpoints
 const get = async (endpoint = "", url = "", username = "") => {
-    const data = await wump(`${baseURL}${endpoint}?url=${url}?username=${username}`).send();
+    username = encodeURIComponent(username);
+
+    const data = await wump(`${baseURL}${endpoint}?url=${url}&username=${username}`).send();
 
     if (data.statusCode !== 200) {
         throw new Err(data);
@@ -17,4 +33,6 @@ const get = async (endpoint = "", url = "", username = "") => {
 };
 
 
-module.exports = get;
+module.exports = {
+    get, endpoints
+};
